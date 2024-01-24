ff0652408119231cdea6e6cd37f65c4db62a7757
Harshit Gupta <104277488+Harshit69P@users.noreply.github.com>
2024-01-24 14:10:36 +0530

    Update userValidationSchema.js

--- v1/validations/userValidationSchema.js
+++ v1/validations/userValidationSchema.js
@@ -9,19 +9,24 @@
 // module.exports = {validate};
 const Joi = require('@hapi/joi');
 
-// Register User and Login User
+// Register User 
 module.exports.validateUser = Joi.object({
   username: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().lowercase().required(),
   password: Joi.string().min(6).max(30).required(),
 });
 
+// and Login User
+module.exports.validateLogin = Joi.object({
+  email: Joi.string().email().lowercase().required(),
+  password: Joi.string().min(6).max(30).required(),
+});
 
 // Get User, Get Users, Delete User, and Update User
 // Assuming 'id' is used to identify the user
-// const validateUserId = Joi.object({
-//   id: Joi.string().required(),
-// });
+module.exports.validateId = Joi.object({
+  id: Joi.string().required(),
+});
 
 
 // module.exports = { validateUser };
