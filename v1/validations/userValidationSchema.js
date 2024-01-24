be9ee22f93ac28e0e5b2b848df50a93e5b4f7cef
Harshit Gupta <104277488+Harshit69P@users.noreply.github.com>
2024-01-24 14:22:10 +0530

    Update userValidationSchema.js

--- v1/validations/userValidationSchema.js
+++ v1/validations/userValidationSchema.js
@@ -22,11 +22,17 @@ module.exports.validateLogin = Joi.object({
   password: Joi.string().min(6).max(30).required(),
 });
 
-// Get User, Get Users, Delete User, and Update User
+// Update User
+module.exports.validateUpdate = Joi.object({
+  username: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().lowercase().required(),
+  password: Joi.string().min(6).max(30).required(),
+});
+
+// Get User, Get Users, Delete User
 // Assuming 'id' is used to identify the user
 module.exports.validateId = Joi.object({
   id: Joi.string().required(),
-});
-
+})
 
 // module.exports = { validateUser };
